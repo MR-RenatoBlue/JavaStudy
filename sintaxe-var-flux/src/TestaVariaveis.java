3ff76f83c72c8db5bd0b7cb0e39fb1154ae4d420
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2022-06-26 15:27:18 -0300

    first commit

---
+++ sintaxe-var-flux/src/TestaVariaveis.java
@@ -0,0 +1,12 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("ola mundo");
+		
+		int idade;
+		idade = 26;
+		
+		System.out.println("Minha idade é: " + idade);
+	}
+}
