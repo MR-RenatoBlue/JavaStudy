57a67dbf2a3d143ec5e04399e094ea7805d43738
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2022-08-21 22:26:53 -0300

    Just did it the ArrayList and Generics class

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/Teste.java
@@ -1,10 +1,56 @@
 package br.com.bytebank.banco.teste.util;
 
+import java.util.ArrayList;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Generics
+		ArrayList<Conta> lista = new ArrayList<Conta>();
+		
+		ArrayList<String> nomes = new ArrayList<String>();
+		
+		Conta cc = new ContaCorrente(22, 11);
+		lista.add(cc);
+		
+		Conta cp = new ContaPoupanca(22, 11);
+		lista.add(cp);
+		
 
+		System.out.println("Tamanho atual: " + lista.size());
+		
+		Conta ref = lista.get(0);
+		
+		System.out.println(ref.getNumero());
+		
+		lista.remove(0);
+		
+		System.out.println("Tamanho atual: " + lista.size());
+	
+	
+		Conta cc1 = new ContaCorrente(22, 11);
+		lista.add(cc);
+		
+		Conta cp1 = new ContaPoupanca(22, 11);
+		lista.add(cp);
+		
+		
+		for (int i = 0; i < lista.size(); i++) {
+			Object oRef = lista.get(i);
+			System.out.println(oRef);
+		}
+		
+		System.out.println("-----------------------------------------------------");
+		
+		for(Object Conta : lista) {
+			System.out.println(Conta);
+		}
+	
 	}
 
 }
