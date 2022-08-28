25c33b7c5e34f7c0369dbbbb551be5a5195bb332
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2022-08-27 22:26:27 -0300

    testing out some IO stuff

--- java-io/src/br/com/alura/io/test/TesteCopia.java
+++ java-io/src/br/com/alura/io/test/TesteCopia.java
@@ -15,21 +15,22 @@ public class TesteCopia {
 
 	public static void main(String[] args) throws Exception {
 
-		InputStream fis = new FileInputStream("input.txt");
+		InputStream fis = System.in;
 		Reader isr = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(isr);
 		
 
-		OutputStream fos = new FileOutputStream("output.txt");
+		OutputStream fos = new FileOutputStream("output.html");
 		Writer osw = new OutputStreamWriter(fos);
 		BufferedWriter bw = new BufferedWriter(osw);
 		
 		
 		String linha = br.readLine();
 			
-		while(linha != null) {
+		while(linha != null && !linha.isEmpty()) {
 			bw.write(linha);
 			bw.newLine();
+			bw.flush();
 			linha = br.readLine();
 		}
 			
@@ -39,3 +40,4 @@ public class TesteCopia {
 	}
 
 }
+//<meta http-equiv="refresh" content="5; URL=file:///home/mrblue/Projects/java/java-io/output.html">
